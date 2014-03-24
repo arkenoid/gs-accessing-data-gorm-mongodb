39722435e1c1142f7879df47dc2233bb048faf30
Graeme Rocher <graeme.rocher@gmail.com>
2014-03-24 17:44:45 +0100

    formatting / cleanup

--- script/app.groovy
+++ script/app.groovy
@@ -3,9 +3,6 @@ import grails.persistence.*
 import grails.mongodb.geo.*
 import org.bson.types.ObjectId
 import com.mongodb.BasicDBObject
-import com.mongodb.Mongo
-import org.springframework.http.*
-import org.springframework.beans.factory.annotation.Autowired
 import static org.springframework.web.bind.annotation.RequestMethod.*
 
 @RestController
@@ -28,22 +25,19 @@ class CityController {
         }
     }
 
-    @Autowired
-    Mongo mongo
-
     @PostConstruct
     void populateCities() {
         City.withTransaction{
             City.collection.remove(new BasicDBObject())
             City.saveAll( 
-                [ new City( name:"London", 
-                            location: Point.valueOf( [-0.125487, 51.508515])),
-                  new City( name:"Paris", 
-                            location: Point.valueOf( [2.352222, 48.856614])),
-                  new City( name:"New York", 
-                            location: Point.valueOf( [-74.005973, 40.714353])),
-                  new City( name:"San Francisco", 
-                            location: Point.valueOf( [-122.419416, 37.774929])) 
+                [ new City(name:"London", 
+                           location: Point.valueOf([-0.125487, 51.508515])),
+                  new City(name:"Paris",
+                           location: Point.valueOf([2.352222, 48.856614])),
+                  new City(name:"New York",
+                           location: Point.valueOf([-74.005973, 40.714353])),
+                  new City(name:"San Francisco",
+                           location: Point.valueOf([-122.419416, 37.774929]))
                 ] 
             )
         }
